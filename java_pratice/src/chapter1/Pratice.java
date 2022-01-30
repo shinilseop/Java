87c8fc536078813c0027ad173add9cbfa2e5ef9a
shinilseop <shinilseop12@gmail.com>
2022-01-30 22:59:50 +0900

    java_bible_chapter2_start
    variable

---
+++ java_pratice/src/chapter1/Pratice.java
@@ -0,0 +1,37 @@
+package chapter1;
+
+public class Pratice {
+    public static void main(String args[]){
+        /**
+         * Variable
+         */
+        int year = 0;
+        int age = 14;
+
+        System.out.println(year);
+        System.out.println(age);
+
+        year = age+2000;
+        age = age+1;
+
+        System.out.println(year);
+        System.out.println(age);
+
+        /**
+         * Swap
+         */
+
+        int x = 10, y = 20;
+        int tmp = 0;
+
+        System.out.println("x:"+x+" y:"+y);
+
+        tmp = x;
+        x = y;
+        y = tmp;
+
+        System.out.println("x:"+x+" y:"+y);
+
+        
+    }
+}
